8ebe17d370e69111d531daf13ef440a8723f4e62
Darkarotte <The127@users.noreply.github.com>
2025-08-12 22:11:52 +0200

    reorganize http handlers

--- handlers/health.go
+++ handlers/health.go
@@ -2,6 +2,10 @@ package handlers
 
 import "net/http"
 
+func ApplicationHealth(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
 func VirtualServerHealth(w http.ResponseWriter, r *http.Request) {
 	// get the current virtual server
 	// check if the vs has registration enabled
