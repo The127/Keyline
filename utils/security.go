6caa6095988b14c508a77c3afb6bcf12e72cfa4d
Darkarotte <The127@users.noreply.github.com>
2025-08-13 13:09:10 +0200

    add util functions for signing key generation

---
+++ utils/security.go
@@ -0,0 +1,30 @@
+package utils
+
+import (
+	"crypto/ed25519"
+	"crypto/rand"
+	"fmt"
+)
+
+func GenerateKeyPair() (ed25519.PrivateKey, ed25519.PublicKey) {
+	publicKey, privateKey, err := ed25519.GenerateKey(rand.Reader)
+	if err != nil {
+		panic(fmt.Errorf("failed to generate key pair: %w", err))
+	}
+	return privateKey, publicKey
+}
+
+func ExportPrivateKey(privateKey ed25519.PrivateKey) []byte {
+	return privateKey
+}
+
+func ImportPrivateKey(privateKeyBytes []byte) (ed25519.PrivateKey, ed25519.PublicKey) {
+	if len(privateKeyBytes) != ed25519.PrivateKeySize {
+		panic(fmt.Errorf("invalid private key size: expected %d bytes, got %d bytes", ed25519.PrivateKeySize, len(privateKeyBytes)))
+	}
+
+	privateKey := ed25519.PrivateKey(privateKeyBytes)
+	publicKey := privateKey.Public().(ed25519.PublicKey)
+
+	return privateKey, publicKey
+}
