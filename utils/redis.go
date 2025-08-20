431b78ac87ef3271d20ed27ec6251cd5c9dea910
karo <karolin.kostial@gmail.com>
2025-08-20 21:58:24 +0200

    extract redis client init function

---
+++ utils/redis.go
@@ -0,0 +1,16 @@
+package utils
+
+import (
+	"Keyline/config"
+	"fmt"
+	"github.com/redis/go-redis/v9"
+)
+
+func NewRedisClient() *redis.Client {
+	return redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("%s:%d", config.C.Redis.Host, config.C.Redis.Port),
+		Username: config.C.Redis.Username,
+		Password: config.C.Redis.Password,
+		DB:       config.C.Redis.Database,
+	})
+}
