b53339f3288f0a58d3f9a33a18b6a97555714ada
karo <karolin.kostial@gmail.com>
2025-08-20 12:42:15 +0200

    implement basic outbox sending job (missing sending to rabbit mq)

--- jobs/OutboxJob.go
+++ jobs/OutboxJob.go
@@ -2,10 +2,12 @@ package jobs
 
 import (
 	"Keyline/ioc"
+	"Keyline/logging"
 	"Keyline/middlewares"
 	"Keyline/repositories"
 	"Keyline/utils"
 	"context"
+	"fmt"
 )
 
 func OutboxSendingJob(dp *ioc.DependencyProvider) JobFn {
@@ -15,8 +17,31 @@ func OutboxSendingJob(dp *ioc.DependencyProvider) JobFn {
 		ctx = middlewares.ContextWithScope(ctx, scope)
 
 		outboxMessageRepository := ioc.GetDependency[*repositories.OutboxMessageRepository](scope)
-		outboxMessageRepository.List(ctx)
+		filter := repositories.NewOutboxMessageFilter()
+		outboxMessages, err := outboxMessageRepository.List(ctx, filter)
+		if err != nil {
+			return fmt.Errorf("failed to list outbox messages: %w", err)
+		}
+
+		for _, message := range outboxMessages {
+			err = handleMessage(ctx, message, outboxMessageRepository)
+			if err != nil {
+				logging.Logger.Errorf("failed handling message: %v", err)
+			}
+		}
 
 		return nil
 	}
+}
+
+func handleMessage(ctx context.Context, message repositories.OutboxMessage, repository *repositories.OutboxMessageRepository) error {
+	// TODO: send to rabbitmq
+
+	filter := repositories.NewOutboxMessageFilter().Id(message.Id())
+	err := repository.Delete(ctx, filter)
+	if err != nil {
+		return fmt.Errorf("failed to delete message in database: %w", err)
+	}
+
+	return nil
 }
