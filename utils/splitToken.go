a6f02be4aa7f6bf0b99f3d4c5ecb224778fbb3c7
karo <karolin.kostial@gmail.com>
2025-08-20 14:55:48 +0200

    add utilities to handle split tokens

---
+++ utils/splitToken.go
@@ -0,0 +1,59 @@
+package utils
+
+import (
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"strings"
+)
+
+type SplitToken struct {
+	id     string
+	secret string
+}
+
+var (
+	ErrDecodingToken = errors.New("error decoding token")
+)
+
+func DecodeSplitToken(base64Token string) (SplitToken, error) {
+	decodedBytes, err := base64.RawURLEncoding.DecodeString(base64Token)
+	if err != nil {
+		return SplitToken{}, fmt.Errorf("base64 decoding token: %w", err)
+	}
+
+	decodedString := string(decodedBytes)
+	tokenParts := strings.Split(decodedString, ":")
+	if len(tokenParts) != 2 {
+		return SplitToken{}, fmt.Errorf("token has not exactly two parts: %w", ErrDecodingToken)
+	}
+
+	token := NewSplitToken(tokenParts[0], tokenParts[1])
+	if token.id == "" {
+		return SplitToken{}, fmt.Errorf("missing id: %w", ErrDecodingToken)
+	}
+	if token.secret == "" {
+		return SplitToken{}, fmt.Errorf("missing secret: %w", ErrDecodingToken)
+	}
+
+	return token, nil
+}
+
+func NewSplitToken(id string, secret string) SplitToken {
+	return SplitToken{
+		id:     id,
+		secret: secret,
+	}
+}
+
+func (t *SplitToken) Encode() string {
+	return fmt.Sprintf("%s:%s", t.id, t.secret)
+}
+
+func (t *SplitToken) Id() string {
+	return t.id
+}
+
+func (t *SplitToken) Secret() string {
+	return t.secret
+}
