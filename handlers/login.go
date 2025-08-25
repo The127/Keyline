071a93b465b2cb6bf42f65803612eecf66ed9bb4
Darkarotte <The127@users.noreply.github.com>
2025-08-25 13:52:30 +0200

    update login state handler to include token validation logic and implement response serialization

--- handlers/login.go
+++ handlers/login.go
@@ -1,7 +1,49 @@
 package handlers
 
-import "net/http"
+import (
+	"Keyline/ioc"
+	"Keyline/middlewares"
+	"Keyline/services"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+type GetLoginStateResponseDto struct {
+	Step string `json:"step"`
+}
 
 func GetLoginState(w http.ResponseWriter, r *http.Request) {
-	// TODO: implement me
+	ctx := r.Context()
+	scope := middlewares.GetScope(ctx)
+
+	vars := mux.Vars(r)
+	loginToken := vars["loginToken"]
+
+	tokenService := ioc.GetDependency[services.TokenService](scope)
+	redisValueString, err := tokenService.GetToken(ctx, services.LoginSessionTokenType, loginToken)
+	if err != nil {
+		http.Error(w, "invalid login token", http.StatusBadRequest)
+		return
+	}
+
+	var loginInfo LoginInfo
+	err = json.Unmarshal([]byte(redisValueString), &loginInfo)
+	if err != nil {
+		http.Error(w, "invalid login token", http.StatusBadRequest)
+		return
+	}
+
+	response := GetLoginStateResponseDto{
+		Step: string(loginInfo.Step),
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	err = json.NewEncoder(w).Encode(response)
+	if err != nil {
+		http.Error(w, "invalid login token", http.StatusBadRequest)
+		return
+	}
 }
