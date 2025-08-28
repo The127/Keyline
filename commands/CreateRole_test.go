9ece58f4735796b19d179af1a9d6423f8434b9c6
Darkarotte <The127@users.noreply.github.com>
2025-08-28 22:08:10 +0200

    add unit test for HandleCreateRole to verify role creation logic

---
+++ commands/CreateRole_test.go
@@ -0,0 +1,64 @@
+package commands
+
+import (
+	"Keyline/ioc"
+	"Keyline/mediator"
+	"Keyline/middlewares"
+	"Keyline/repositories"
+	"Keyline/repositories/mocks"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/mock/gomock"
+	"testing"
+	"time"
+)
+
+func TestHandleCreateRole(t *testing.T) {
+	t.Parallel()
+
+	// arrange
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	virtualServer := repositories.NewVirtualServer("virtualServer", "Virtual Server")
+	virtualServerRepository := mocks.NewMockVirtualServerRepository(ctrl)
+	virtualServerRepository.EXPECT().Single(gomock.Any(), gomock.Cond(func(x repositories.VirtualServerFilter) bool {
+		return *x.GetName() == virtualServer.Name()
+	})).Return(virtualServer, nil)
+
+	roleRepository := mocks.NewMockRoleRepository(ctrl)
+	roleRepository.EXPECT().Insert(gomock.Any(), gomock.Cond(func(x *repositories.Role) bool {
+		return x.Name() == "role" &&
+			x.Description() == "description" &&
+			x.VirtualServerId() == virtualServer.Id() &&
+			x.RequireMfa() == true &&
+			*x.MaxTokenAge() == time.Hour
+	}))
+
+	dc := ioc.NewDependencyCollection()
+	ioc.RegisterTransient(dc, func(dp *ioc.DependencyProvider) repositories.VirtualServerRepository {
+		return virtualServerRepository
+	})
+	ioc.RegisterTransient(dc, func(dp *ioc.DependencyProvider) repositories.RoleRepository {
+		return roleRepository
+	})
+	ioc.RegisterTransient(dc, func(dp *ioc.DependencyProvider) *mediator.Mediator {
+		return mediator.NewMediator()
+	})
+	scope := dc.BuildProvider()
+	ctx := middlewares.ContextWithScope(t.Context(), scope)
+
+	cmd := CreateRole{
+		VirtualServerName: virtualServer.Name(),
+		Name:              "role",
+		Description:       "description",
+		RequireMfa:        true,
+		MaxTokenAge:       time.Hour,
+	}
+
+	// act
+	resp, err := HandleCreateRole(ctx, cmd)
+
+	// assert
+	assert.NoError(t, err)
+	assert.NotNil(t, resp)
+}
