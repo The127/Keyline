663b8344c73e6b51d1688ab60ea26d76a50da38a
karo <karolin.kostial@gmail.com>
2025-10-11 08:51:45 +0200

    refactor: remove redundant alias imports, introduce `ClockService`, and update dependency registrations and mediator handlers

---
+++ internal/time/clockservice.go
@@ -0,0 +1,17 @@
+package time
+
+import "time"
+
+type ClockService interface {
+	Now() time.Time
+}
+
+type clockService struct{}
+
+func NewClockService() ClockService {
+	return &clockService{}
+}
+
+func (c *clockService) Now() time.Time {
+	return time.Now()
+}
