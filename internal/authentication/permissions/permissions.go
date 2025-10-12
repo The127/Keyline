dbb16c7088901abb5adc6f1eb562b531ed9b97a5
karo <karolin.kostial@gmail.com>
2025-10-12 20:25:11 +0200

    feat: add `RoleCreate` permission and policy to `CreateRole` command

--- internal/authentication/permissions/permissions.go
+++ internal/authentication/permissions/permissions.go
@@ -7,6 +7,7 @@ const (
 
 	ApplicationCreate Permission = "application:create"
 
+	RoleCreate Permission = "role:create"
 	RoleAssign Permission = "role:assign"
 
 	ServiceUserAssociateKey Permission = "service_user:associate_key"
