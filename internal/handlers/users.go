d189a0a4e6cf6a73d7dc4905c0fc09231478b669
karo <karolin.kostial@gmail.com>
2025-10-12 13:53:03 +0200

    feat: add `GetUserApplicationMetadata` endpoint with OpenAPI documentation and handler implementation

--- internal/handlers/users.go
+++ internal/handlers/users.go
@@ -261,6 +261,71 @@ func GetUserById(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+type GetUserApplicationMetadataResponseDto map[string]any
+
+// GetUserMetadata returns a users application metadata.
+// @Summary      Get users application metadata
+// @Tags         Users
+// @Produce      json
+// @Param        virtualServerName  path  string true  "Virtual server name"  default(keyline)
+// @Param        userId             path  string true  "User ID (UUID)"
+// @Param        appId              path  string true  "Application ID (UUID)"
+// @Success      200  {object}  GetUserApplicationMetadataResponseDto
+// @Failure      404  {string}  string
+// @Router       /api/virtual-servers/{virtualServerName}/users/{userId}/metadata/application/{appId} [get]
+func GetUserApplicationMetadata(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	scope := middlewares.GetScope(ctx)
+
+	vsName, err := middlewares.GetVirtualServerName(ctx)
+	if err != nil {
+		utils.HandleHttpError(w, err)
+		return
+	}
+
+	vars := mux.Vars(r)
+	userId, err := uuid.Parse(vars["userId"])
+	if err != nil {
+		utils.HandleHttpError(w, utils.ErrInvalidUuid)
+		return
+	}
+
+	appId, err := uuid.Parse(vars["appId"])
+	if err != nil {
+		utils.HandleHttpError(w, utils.ErrInvalidUuid)
+		return
+	}
+
+	m := ioc.GetDependency[mediator.Mediator](scope)
+	query := queries.GetUserMetadata{
+		VirtualServerName: vsName,
+		UserId:            userId,
+		ApplicationIds:    utils.Ptr([]uuid.UUID{appId}),
+	}
+	response, err := mediator.Send[*queries.GetUserMetadataResult](ctx, m, query)
+	if err != nil {
+		utils.HandleHttpError(w, err)
+		return
+	}
+
+	var responseDto GetUserGlobalMetadataResponseDto = make(map[string]any)
+
+	for _, v := range response.ApplicationMetadata {
+		err := json.Unmarshal([]byte(v), &responseDto)
+		if err != nil {
+			utils.HandleHttpError(w, err)
+			return
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	err = json.NewEncoder(w).Encode(responseDto)
+	if err != nil {
+		utils.HandleHttpError(w, err)
+	}
+}
+
 type GetUserGlobalMetadataResponseDto map[string]any
 
 // GetUserMetadata returns a users metadata (only the global metadata).
