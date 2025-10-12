c3a97d3ab38b083e05a35d2cfa7c02aa254e53c9
karo <karolin.kostial@gmail.com>
2025-10-12 21:36:38 +0200

    fix: correct error formatting in panic recovery logging

--- internal/middlewares/RecoverMiddleware.go
+++ internal/middlewares/RecoverMiddleware.go
@@ -12,7 +12,7 @@ func RecoverMiddleware() mux.MiddlewareFunc {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			defer func() {
 				if err := recover(); err != nil {
-					logging.Logger.Errorf("recovering from handler panic: %w", err)
+					logging.Logger.Errorf("recovering from handler panic: %v", err)
 					http.Error(w, "internal server error", http.StatusInternalServerError)
 				}
 			}()
