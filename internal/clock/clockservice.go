876d1a5f3b4e3e7883bbd993c54bbf44c0562c02
karo <karolin.kostial@gmail.com>
2025-10-16 18:19:03 +0200

    refactor: add `mockService` and constructors to support testing for `clockservice`

--- internal/clock/clockservice.go
+++ internal/clock/clockservice.go
@@ -6,6 +6,24 @@ type Service interface {
 	Now() time.Time
 }
 
+type mockService struct {
+	now time.Time
+}
+
+func (m *mockService) Now() time.Time {
+	return m.now
+}
+
+func NewMockServiceNow() Service {
+	return NewMockService(time.Now())
+}
+
+func NewMockService(now time.Time) Service {
+	return &mockService{
+		now: now,
+	}
+}
+
 type clockService struct{}
 
 func NewClockService() Service {
