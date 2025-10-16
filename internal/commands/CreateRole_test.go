83a055484c5d644afdccfc116e068b13d8048c91
karo <karolin.kostial@gmail.com>
2025-10-16 20:25:53 +0200

    test: extract context creation in `CreateRole` tests to reusable helper for reducing duplication

--- internal/commands/CreateRole_test.go
+++ internal/commands/CreateRole_test.go
@@ -1,11 +1,15 @@
 package commands
 
 import (
+	"Keyline/internal/events"
 	"Keyline/internal/middlewares"
 	"Keyline/internal/repositories"
 	"Keyline/internal/repositories/mocks"
 	"Keyline/ioc"
 	"Keyline/mediator"
+	mediatorMocks "Keyline/mediator/mocks"
+	"Keyline/utils"
+	"context"
 	"testing"
 	"time"
 
@@ -20,6 +24,39 @@ type CreateRoleCommandSuite struct {
 func TestCreateRoleCommandSuite(t *testing.T) {
 	t.Parallel()
 	suite.Run(t, new(CreateRoleCommandSuite))
+}
+
+func (s *CreateRoleCommandSuite) createContext(
+	vsr repositories.VirtualServerRepository,
+	rr repositories.RoleRepository,
+	m mediator.Mediator,
+) context.Context {
+	dc := ioc.NewDependencyCollection()
+
+	if vsr != nil {
+		ioc.RegisterTransient(dc, func(_ *ioc.DependencyProvider) repositories.VirtualServerRepository {
+			return vsr
+		})
+	}
+
+	if rr != nil {
+		ioc.RegisterTransient(dc, func(_ *ioc.DependencyProvider) repositories.RoleRepository {
+			return rr
+		})
+	}
+
+	if m != nil {
+		ioc.RegisterTransient(dc, func(_ *ioc.DependencyProvider) mediator.Mediator {
+			return m
+		})
+	}
+
+	scope := dc.BuildProvider()
+	s.T().Cleanup(func() {
+		utils.PanicOnError(scope.Close, "closing scope")
+	})
+
+	return middlewares.ContextWithScope(s.T().Context(), scope)
 }
 
 func (s *CreateRoleCommandSuite) TestHappyPath() {
@@ -42,19 +79,10 @@ func (s *CreateRoleCommandSuite) TestHappyPath() {
 			*x.MaxTokenAge() == time.Hour
 	}))
 
-	dc := ioc.NewDependencyCollection()
-	ioc.RegisterTransient(dc, func(dp *ioc.DependencyProvider) repositories.VirtualServerRepository {
-		return virtualServerRepository
-	})
-	ioc.RegisterTransient(dc, func(dp *ioc.DependencyProvider) repositories.RoleRepository {
-		return roleRepository
-	})
-	ioc.RegisterTransient(dc, func(dp *ioc.DependencyProvider) mediator.Mediator {
-		return mediator.NewMediator()
-	})
-	scope := dc.BuildProvider()
-	ctx := middlewares.ContextWithScope(s.T().Context(), scope)
+	m := mediatorMocks.NewMockMediator(ctrl)
+	m.EXPECT().SendEvent(gomock.Any(), gomock.AssignableToTypeOf(events.RoleCreatedEvent{}), gomock.Any())
 
+	ctx := s.createContext(virtualServerRepository, roleRepository, m)
 	cmd := CreateRole{
 		VirtualServerName: virtualServer.Name(),
 		Name:              "role",
