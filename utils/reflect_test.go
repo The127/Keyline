e7d8447dd6fb586e677265c0f1ccb38a7d62a0f8
karo <karolin.kostial@gmail.com>
2025-10-16 23:31:21 +0200

    test: add `Zero` utility tests using testify suite for structured validation of zero-value behavior

---
+++ utils/reflect_test.go
@@ -0,0 +1,44 @@
+package utils
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+type ZeroSuite struct {
+	suite.Suite
+}
+
+func TestZeroSuite(t *testing.T) {
+	t.Parallel()
+	suite.Run(t, new(ZeroSuite))
+}
+
+func (s *ZeroSuite) TestPrimitive() {
+	// act
+	zero := Zero[int]()
+
+	// assert
+	s.Equal(0, zero)
+}
+
+func (s *ZeroSuite) TestString() {
+	// act
+	zero := Zero[string]()
+
+	// assert
+	s.Equal("", zero)
+}
+
+func (s *ZeroSuite) TestStruct() {
+	// act
+	zero := Zero[struct {
+		Field string
+	}]()
+
+	//assert
+	s.Equal(struct {
+		Field string
+	}{}, zero)
+}
