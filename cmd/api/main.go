72a9d6e5bf9367b14f7ccf2e754f54cf1dd3d298
karo <karolin.kostial@gmail.com>
2025-10-17 14:45:39 +0200

    refactor: extract mediator setup logic to `internal/setup` for improved modularity and reuse

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -17,7 +17,6 @@ import (
 	"Keyline/internal/commands"
 	"Keyline/internal/config"
 	"Keyline/internal/database"
-	"Keyline/internal/events"
 	"Keyline/internal/jobs"
 	"Keyline/internal/logging"
 	"Keyline/internal/metrics"
@@ -30,6 +29,7 @@ import (
 	"Keyline/internal/services/audit"
 	"Keyline/internal/services/claimsMapping"
 	"Keyline/internal/services/keyValue"
+	"Keyline/internal/setup"
 	"Keyline/ioc"
 	"Keyline/mediator"
 	"Keyline/utils"
@@ -187,7 +187,7 @@ func main() {
 		return postgres.NewAuditLogRepository()
 	})
 
-	setupMediator(dc)
+	setup.SetupMediator(dc)
 	dp := dc.BuildProvider()
 
 	jobManager := jobs.NewJobManager(jobs.WithOnError(func(err error) {
@@ -210,57 +210,6 @@ func main() {
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	<-c
-}
-
-func setupMediator(dc *ioc.DependencyCollection) {
-	m := mediator.NewMediator()
-
-	mediator.RegisterHandler(m, queries.HandleAnyVirtualServerExists)
-	mediator.RegisterHandler(m, queries.HandleGetVirtualServerPublicInfo)
-	mediator.RegisterHandler(m, queries.HandleGetVirtualServerQuery)
-	mediator.RegisterHandler(m, commands.HandleCreateVirtualServer)
-
-	mediator.RegisterHandler(m, queries.HandleListTemplates)
-	mediator.RegisterHandler(m, queries.HandleGetTemplate)
-
-	mediator.RegisterHandler(m, commands.HandleRegisterUser)
-	mediator.RegisterHandler(m, commands.HandleCreateUser)
-	mediator.RegisterHandler(m, commands.HandleVerifyEmail)
-	mediator.RegisterHandler(m, commands.HandleResetPassword)
-	mediator.RegisterHandler(m, queries.HandleGetUserQuery)
-	mediator.RegisterHandler(m, commands.HandlePatchUser)
-	mediator.RegisterHandler(m, queries.HandleListUsers)
-	mediator.RegisterHandler(m, commands.HandleCreateServiceUser)
-	mediator.RegisterHandler(m, commands.HandleAssociateServiceUserPublicKey)
-	mediator.RegisterHandler(m, queries.HandleGetUserMetadata)
-	mediator.RegisterHandler(m, commands.HandleUpdateUserMetadata)
-	mediator.RegisterHandler(m, commands.HandleUpdateUserAppMetadata)
-	mediator.RegisterHandler(m, commands.HandlePatchUserMetadata)
-	mediator.RegisterHandler(m, commands.HandlePatchUserAppMetadata)
-
-	mediator.RegisterHandler(m, commands.HandleCreateApplication)
-	mediator.RegisterHandler(m, queries.HandleListApplications)
-	mediator.RegisterHandler(m, queries.HandleGetApplication)
-	mediator.RegisterHandler(m, commands.HandlePatchApplication)
-	mediator.RegisterHandler(m, commands.HandleDeleteApplication)
-
-	mediator.RegisterHandler(m, queries.HandleListRoles)
-	mediator.RegisterHandler(m, queries.HandleGetRole)
-	mediator.RegisterHandler(m, commands.HandleCreateRole)
-	mediator.RegisterHandler(m, commands.HandleAssignRoleToUser)
-	mediator.RegisterHandler(m, queries.HandleListUsersInRole)
-
-	mediator.RegisterHandler(m, queries.HandleListGroups)
-
-	mediator.RegisterHandler(m, queries.HandleListAuditEntries)
-
-	mediator.RegisterEventHandler(m, events.QueueEmailVerificationJobOnUserCreatedEvent)
-
-	mediator.RegisterBehaviour(m, behaviours.PolicyBehaviour)
-
-	ioc.RegisterSingleton(dc, func(dp *ioc.DependencyProvider) mediator.Mediator {
-		return m
-	})
 }
 
 // initApplication sets up the initial application state on the first startup.
