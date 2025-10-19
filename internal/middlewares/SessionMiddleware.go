98b351cf47200852199c64b5d67f6499afb2d9b7
Kristian Jakobsen <kristian@nymann.dev>
2025-10-19 20:02:14 +0200

    Parse the hostname from the externalUrl and use that as domain for the cookie
    Co-authored-by: karo <karolin.kostial@gmail.com>
    Signed-off-by: Kristian Jakobsen <kristian@nymann.dev>

--- internal/middlewares/SessionMiddleware.go
+++ internal/middlewares/SessionMiddleware.go
@@ -8,6 +8,7 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"net/url"
 	"time"
 
 	"github.com/google/uuid"
@@ -149,11 +150,15 @@ func CreateSession(w http.ResponseWriter, r *http.Request, vsName string, userId
 }
 
 func setCookie(w http.ResponseWriter, name string, value string, maxAge int) {
+	externalUrl, err := url.Parse(config.C.Server.ExternalUrl)
+	if err != nil {
+		panic(err)
+	}
 	cookie := http.Cookie{
 		Name:     name,
 		Value:    value,
 		Path:     "/",
-		Domain:   config.C.Server.ExternalUrl,
+		Domain:   externalUrl.Hostname(),
 		MaxAge:   maxAge,
 		Secure:   true,
 		HttpOnly: true,
