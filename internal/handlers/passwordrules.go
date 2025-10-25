211f8ea91d2f9abab2cd558cbb7bd6f357ba5f98
karo <karolin.kostial@gmail.com>
2025-10-25 14:28:30 +0200

    feat: enhance password policy serialization and add rule-type support
    Added `GetPasswordRuleType` and `Serialize` methods to all password policy structs, enabling rule type association and JSON serialization. Introduced `CreatePasswordRule` API handler to process rule creation requests.
    
    Signed-off-by: karo <karolin.kostial@gmail.com>

--- internal/handlers/passwordrules.go
+++ internal/handlers/passwordrules.go
@@ -1,8 +1,10 @@
 package handlers
 
 import (
+	"Keyline/internal/commands"
 	"Keyline/internal/middlewares"
 	"Keyline/internal/queries"
+	"Keyline/internal/repositories"
 	"Keyline/ioc"
 	"Keyline/mediator"
 	"Keyline/utils"
@@ -79,8 +81,46 @@ func ListPasswordRules(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func CreatePasswordRule(w http.ResponseWriter, r *http.Request) {
+type CreatePasswordRuleRequestDto struct {
+	Type    string                 `json:"type" validate:"required"`
+	Details map[string]interface{} `json:"details" validate:"required"`
+}
 
+func CreatePasswordRule(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	vsName, err := middlewares.GetVirtualServerName(ctx)
+	if err != nil {
+		utils.HandleHttpError(w, err)
+		return
+	}
+
+	var requestDto CreatePasswordRuleRequestDto
+	err = json.NewDecoder(r.Body).Decode(&requestDto)
+	if err != nil {
+		utils.HandleHttpError(w, err)
+	}
+
+	err = utils.ValidateDto(requestDto)
+	if err != nil {
+		utils.HandleHttpError(w, err)
+		return
+	}
+
+	scope := middlewares.GetScope(ctx)
+	m := ioc.GetDependency[mediator.Mediator](scope)
+
+	_, err = mediator.Send[*commands.CreatePasswordRuleResponse](ctx, m, commands.CreatePasswordRule{
+		VirtualServerName: vsName,
+		Type:              repositories.PasswordRuleType(requestDto.Type),
+		Details:           requestDto.Details,
+	})
+	if err != nil {
+		utils.HandleHttpError(w, err)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
 }
 
 func GetPasswordRule(w http.ResponseWriter, r *http.Request) {
