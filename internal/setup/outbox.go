5da3bd68ffe06d38152ba2459f224bb89205bd7e
karo <karolin.kostial@gmail.com>
2025-10-26 14:52:51 +0100

    refactor: extract OutboxDelivery to standalone file
    Moved the `OutboxDelivery` function from `setup.go` to a new `outbox.go` file under the internal setup package for improved maintainability and separation of concerns.
    
    Signed-off-by: karo <karolin.kostial@gmail.com>

---
+++ internal/setup/outbox.go
@@ -0,0 +1,28 @@
+package setup
+
+import (
+	"Keyline/internal/config"
+	"Keyline/internal/services/outbox"
+	"Keyline/ioc"
+)
+
+func OutboxDelivery(dc *ioc.DependencyCollection, queueMode config.QueueMode) {
+	switch queueMode {
+	case config.QueueModeNoop:
+		ioc.RegisterSingleton(dc, func(_ *ioc.DependencyProvider) outbox.DeliveryService {
+			return outbox.NewNoopDeliveryService()
+		})
+
+	case config.QueueModeInProcess:
+		ioc.RegisterSingleton(dc, func(_ *ioc.DependencyProvider) outbox.DeliveryService {
+			return outbox.NewInProcessDeliveryService()
+		})
+
+	default:
+		panic("queue mode missing or not supported")
+	}
+
+	ioc.RegisterTransient(dc, func(dp *ioc.DependencyProvider) outbox.MessageBroker {
+		return outbox.NewMessageBroker()
+	})
+}
