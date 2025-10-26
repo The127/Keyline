84d4883a8415b63ea41b391d688b30bf995af923
karo <karolin.kostial@gmail.com>
2025-10-26 08:39:43 +0100

    feat: add projects table and repository
    Introduced a `projects` table with migrations, including constraints and indexes. Added a repository for managing projects, filter support, and a corresponding mock implementation for testing.
    
    Signed-off-by: karo <karolin.kostial@gmail.com>

---
+++ internal/database/migrations/33_addProjects.sql
@@ -0,0 +1,32 @@
+-- +migrate Up
+
+create table projects (
+    "id" uuid not null default gen_random_uuid(),
+    "audit_created_at" timestamp not null default now(),
+    "audit_updated_at" timestamp not null default now(),
+    "version" bigint not null default 1,
+
+    "virtual_server_id" uuid not null,
+
+    "slug" text not null,
+    "name" text not null,
+    "description" text not null,
+
+    primary key ("id")
+);
+
+alter table "projects"
+    add constraint "fk_projects_virtual_server_id"
+    foreign key ("virtual_server_id")
+    references "virtual_servers" ("id");
+
+create unique index "idx_projects_virtual_server_id_slug" on "projects" ("virtual_server_id", "slug");
+
+create trigger "trg_set_audit_updated_at"
+    before update
+    on "projects"
+    for each row
+    execute function update_audit_timestamp();
+
+-- +migrate Down
+
