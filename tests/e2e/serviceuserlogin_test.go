d15fc2375d351cbb162ded98fd1bb282bd262173
karo <karolin.kostial@gmail.com>
2025-11-04 22:18:39 +0100

    test: add e2e test for invalid service user private key flow
    Added a test to validate the system's behavior when a service user attempts login with an invalid private key. Ensured the response status code is not OK and updated assertions for robustness.
    
    Signed-off-by: karo <karolin.kostial@gmail.com>

--- tests/e2e/serviceuserlogin_test.go
+++ tests/e2e/serviceuserlogin_test.go
@@ -17,7 +17,7 @@ import (
 var _ = Describe("Service user login", Ordered, func() {
 	var h *harness
 
-	const wrongPrivateKey = "-----BEGIN PRIVATE KEY-----\nMFECAQEwBQYDK2VwBCIEINDNz/ANPjGVanCJq0FOZOj3NdSKsjhNk+i4CIuaszVm\ngSEAQldUwS8V3nUSwBfvU4NMkC6UM7x0plXqZUuU/gcNG+k=\n-----END PRIVATE KEY-----\n"
+	const wrongPrivateKey = "-----BEGIN PRIVATE KEY-----\nMFECAQEwBQYDK2VwBCIEIGlwiAvmqJTxz8n1Ewwwy7/XG2LJphqbOhfKcfg2l9YU\ngSEAi+MvQpVxlYdQrbbsn5xltPOYbU00qJtkEHPO2uzUmKQ=\n-----END PRIVATE KEY-----\n"
 
 	BeforeAll(func() {
 		h = newE2eTestHarness(nil)
@@ -25,6 +25,37 @@ var _ = Describe("Service user login", Ordered, func() {
 
 	AfterAll(func() {
 		h.Close()
+	})
+
+	It("fails with wrong private key", func() {
+		block, _ := pem.Decode([]byte(wrongPrivateKey))
+		if block == nil {
+			panic("failed to decode PEM")
+		}
+
+		key, err := x509.ParsePKCS8PrivateKey(block.Bytes)
+		Expect(err).ToNot(HaveOccurred())
+
+		claims := jwt.MapClaims{
+			"aud":    commands.AdminApplicationName,
+			"iss":    h.ServiceUserId().String(),
+			"sub":    h.ServiceUserId().String(),
+			"scopes": "openid profile email",
+		}
+		jwtToken := jwt.NewWithClaims(jwt.SigningMethodEdDSA, claims)
+		jwtToken.Header["kid"] = h.ServiceUserKid()
+		signedJWT, err := jwtToken.SignedString(key)
+		Expect(err).ToNot(HaveOccurred())
+
+		resp, err := http.PostForm(fmt.Sprintf("%s/oidc/%s/token", h.ApiUrl(), h.VirtualServer()),
+			url.Values{
+				"grant_type":         {"urn:ietf:params:oauth:grant-type:token-exchange"},
+				"subject_token":      {signedJWT},
+				"subject_token_type": {"urn:ietf:params:oauth:token-type:access_token"},
+			},
+		)
+		Expect(err).ToNot(HaveOccurred())
+		Expect(resp.StatusCode).ToNot(Equal(http.StatusOK))
 	})
 
 	It("exchanges token successfully", func() {
@@ -55,6 +86,7 @@ var _ = Describe("Service user login", Ordered, func() {
 			},
 		)
 		Expect(err).ToNot(HaveOccurred())
+		Expect(resp.StatusCode).To(Equal(http.StatusOK))
 
 		var responseJson map[string]any
 		err = json.NewDecoder(resp.Body).Decode(
