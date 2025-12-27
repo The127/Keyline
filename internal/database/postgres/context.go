4f2cc413352e6d2feec98b1a87305bd2e5a2c6da
karo <karolin.kostial@gmail.com>
2025-12-27 10:59:10 +0100

    implement transaction handling and change application in database.Context
    Added `SaveChanges` method with transactional support and `applyChange` logic to handle entity-specific changes in `database.Context`.
    
    Signed-off-by: karo <karolin.kostial@gmail.com>

--- internal/database/postgres/context.go
+++ internal/database/postgres/context.go
@@ -7,6 +7,7 @@ import (
 	"Keyline/internal/repositories/postgres"
 	"context"
 	"database/sql"
+	"fmt"
 )
 
 type Context struct {
@@ -185,6 +186,209 @@ func newContext(db *sql.DB) *Context {
 }
 
 func (c *Context) SaveChanges(ctx context.Context) error {
-	// TODO: implement me
+	tx, err := c.db.BeginTx(ctx, nil)
+	if err != nil {
+		return fmt.Errorf("starting transaction: %w", err)
+	}
+
+	for _, ch := range c.changeTracker.GetChanges() {
+		err := c.applyChange(ctx, tx, ch)
+		if err != nil {
+			return fmt.Errorf("applying change: %w", err)
+		}
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return fmt.Errorf("committing transaction: %w", err)
+	}
+
 	return nil
+}
+
+func (c *Context) applyChange(ctx context.Context, tx *sql.Tx, ch *change.Entry) error {
+	switch ch.GetItemType() {
+	case db.ApplicationEntityType:
+		return c.applyApplicationChange(ctx, tx, ch)
+
+	case db.ApplicationUserMetadataEntityType:
+		return c.applyApplicationUserMetadataChange(ctx, tx, ch)
+
+	case db.AuditLogEntityType:
+		return c.applyAuditLogChange(ctx, tx, ch)
+
+	case db.CredentialEntityType:
+		return c.applyCredentialChange(ctx, tx, ch)
+
+	case db.FileEntityType:
+		return c.applyFileChange(ctx, tx, ch)
+
+	case db.GroupRoleEntityType:
+		return c.applyGroupRoleChange(ctx, tx, ch)
+
+	case db.GroupEntityType:
+		return c.applyGroupChange(ctx, tx, ch)
+
+	case db.OutboxMessageEntityType:
+		return c.applyOutboxMessageChange(ctx, tx, ch)
+
+	case db.PasswordRuleEntityType:
+		return c.applyPasswordRuleChange(ctx, tx, ch)
+
+	case db.ProjectEntityType:
+		return c.applyProjectChange(ctx, tx, ch)
+
+	case db.ResourceServerEntityType:
+		return c.applyResourceServerChange(ctx, tx, ch)
+
+	case db.ResourceServerScopeEntityType:
+		return c.applyResourceServerScopeChange(ctx, tx, ch)
+
+	case db.RoleEntityType:
+		return c.applyRoleChange(ctx, tx, ch)
+
+	case db.SessionEntityType:
+		return c.applySessionChange(ctx, tx, ch)
+
+	case db.TemplateEntityType:
+		return c.applyTemplateChange(ctx, tx, ch)
+
+	case db.UserRoleAssignmentEntityType:
+		return c.applyUserRoleAssignmentChange(ctx, tx, ch)
+
+	case db.UserEntityType:
+		return c.applyUserChange(ctx, tx, ch)
+
+	case db.VirtualServerEntityType:
+		return c.applyVirtualServerChange(ctx, tx, ch)
+
+	default:
+		return fmt.Errorf("unsupported item type: %v", ch.GetItemType())
+	}
+}
+
+func (c *Context) applyApplicationUserMetadataChange(ctx context.Context, tx *sql.Tx, ch *change.Entry) error {
+	switch ch.GetChangeType() {
+	default:
+		return fmt.Errorf("unsupported change type: %v", ch.GetChangeType())
+	}
+}
+
+func (c *Context) applyAuditLogChange(ctx context.Context, tx *sql.Tx, ch *change.Entry) error {
+	switch ch.GetChangeType() {
+	default:
+		return fmt.Errorf("unsupported change type: %v", ch.GetChangeType())
+	}
+}
+
+func (c *Context) applyCredentialChange(ctx context.Context, tx *sql.Tx, ch *change.Entry) error {
+	switch ch.GetChangeType() {
+	default:
+		return fmt.Errorf("unsupported change type: %v", ch.GetChangeType())
+	}
+}
+
+func (c *Context) applyFileChange(ctx context.Context, tx *sql.Tx, ch *change.Entry) error {
+	switch ch.GetChangeType() {
+	default:
+		return fmt.Errorf("unsupported change type: %v", ch.GetChangeType())
+	}
+}
+
+func (c *Context) applyGroupRoleChange(ctx context.Context, tx *sql.Tx, ch *change.Entry) error {
+	switch ch.GetChangeType() {
+	default:
+		return fmt.Errorf("unsupported change type: %v", ch.GetChangeType())
+	}
+}
+
+func (c *Context) applyGroupChange(ctx context.Context, tx *sql.Tx, ch *change.Entry) error {
+	switch ch.GetChangeType() {
+	default:
+		return fmt.Errorf("unsupported change type: %v", ch.GetChangeType())
+	}
+}
+
+func (c *Context) applyOutboxMessageChange(ctx context.Context, tx *sql.Tx, ch *change.Entry) error {
+	switch ch.GetChangeType() {
+	default:
+		return fmt.Errorf("unsupported change type: %v", ch.GetChangeType())
+	}
+}
+
+func (c *Context) applyPasswordRuleChange(ctx context.Context, tx *sql.Tx, ch *change.Entry) error {
+	switch ch.GetChangeType() {
+	default:
+		return fmt.Errorf("unsupported change type: %v", ch.GetChangeType())
+	}
+}
+
+func (c *Context) applyProjectChange(ctx context.Context, tx *sql.Tx, ch *change.Entry) error {
+	switch ch.GetChangeType() {
+	default:
+		return fmt.Errorf("unsupported change type: %v", ch.GetChangeType())
+	}
+}
+
+func (c *Context) applyResourceServerChange(ctx context.Context, tx *sql.Tx, ch *change.Entry) error {
+	switch ch.GetChangeType() {
+	default:
+		return fmt.Errorf("unsupported change type: %v", ch.GetChangeType())
+	}
+}
+
+func (c *Context) applyResourceServerScopeChange(ctx context.Context, tx *sql.Tx, ch *change.Entry) error {
+	switch ch.GetChangeType() {
+	default:
+		return fmt.Errorf("unsupported change type: %v", ch.GetChangeType())
+	}
+}
+
+func (c *Context) applyRoleChange(ctx context.Context, tx *sql.Tx, ch *change.Entry) error {
+	switch ch.GetChangeType() {
+	default:
+		return fmt.Errorf("unsupported change type: %v", ch.GetChangeType())
+	}
+}
+
+func (c *Context) applySessionChange(ctx context.Context, tx *sql.Tx, ch *change.Entry) error {
+	switch ch.GetChangeType() {
+	default:
+		return fmt.Errorf("unsupported change type: %v", ch.GetChangeType())
+	}
+}
+
+func (c *Context) applyTemplateChange(ctx context.Context, tx *sql.Tx, ch *change.Entry) error {
+	switch ch.GetChangeType() {
+	default:
+		return fmt.Errorf("unsupported change type: %v", ch.GetChangeType())
+	}
+}
+
+func (c *Context) applyApplicationChange(ctx context.Context, tx *sql.Tx, ch *change.Entry) error {
+	switch ch.GetChangeType() {
+	default:
+		return fmt.Errorf("unsupported change type: %v", ch.GetChangeType())
+	}
+}
+
+func (c *Context) applyUserRoleAssignmentChange(ctx context.Context, tx *sql.Tx, ch *change.Entry) error {
+	switch ch.GetChangeType() {
+	default:
+		return fmt.Errorf("unsupported change type: %v", ch.GetChangeType())
+	}
+}
+
+func (c *Context) applyUserChange(ctx context.Context, tx *sql.Tx, ch *change.Entry) error {
+	switch ch.GetChangeType() {
+	default:
+		return fmt.Errorf("unsupported change type: %v", ch.GetChangeType())
+	}
+}
+
+func (c *Context) applyVirtualServerChange(ctx context.Context, tx *sql.Tx, ch *change.Entry) error {
+	switch ch.GetChangeType() {
+	default:
+		return fmt.Errorf("unsupported change type: %v", ch.GetChangeType())
+	}
 }
